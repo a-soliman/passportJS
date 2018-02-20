3db97ded494f338b8d1808373903f874a6db12c6
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-20 08:29:24 -0800

    debugged the server issue

--- app.js
+++ app.js
@@ -14,23 +14,27 @@ const mongoose			= require('mongoose');
 mongoose.connect('mongodb://ahmed_soliman:123456@ds143778.mlab.com:43778/passport-auth');
 const db 				= mongoose.connection;
 
-// Routes
-const routes 			= require('./routes/index');
-const users				= require('./routes/users');
-
 // Init App
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
 // View Engine
-app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+// Routes
+const routes 			= require('./routes/index');
+const users				= require('./routes/users');
 
 // BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser);
+app.use(cookieParser());
 
 
 // Public Folder
