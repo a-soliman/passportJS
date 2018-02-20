9d469c0cd2826e690172e956f0aa6aa3f9c19819
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-20 08:29:08 -0800

    started the users route for serving register and login router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 const express		= require('express');
 const router		= express.Router();
 
-//Get homepage
-router.get('/', ( req, res ) => {
-	res.render('users');
+// Register
+router.get('/register', ( req, res ) => {
+	res.render('register');
+});
+
+// Login
+router.get('/login', ( req, res ) => {
+	res.render('login');
 });
 
 module.exports 	= router;
