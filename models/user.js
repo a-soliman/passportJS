67296a65ff96e69da704f948e4dc0c9ae2709ba5
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-20 11:56:04 -0800

    coded createUser function using bcrypt to incriby password

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose		= require('mongoose');
+const bcrypt		= require('bcryptjs');
+
+mongoose.connect('mongodb://ahmed_soliman:123456@ds143778.mlab.com:43778/passport-auth');
+const db 			= mongoose.connection;
+
+// User Schema
+
+const UserSchema = mongoose.Schema({
+	username: {
+		type: String,
+		index: true
+	},
+	password: {
+		type: String
+	},
+	email: {
+		type: String
+	},
+	name: {
+		type: String
+	}
+});
+
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+// the needed function
+module.exports.createUser = ( newUser, callback ) => {
+	bcrypt.genSalt(10, ( err, salt ) => {
+		bcrypt.hash(newUser.password, salt, ( err, hash ) => {
+			newUser.password = hash;
+			newUser.save(callback);
+		});
+	});
+}
