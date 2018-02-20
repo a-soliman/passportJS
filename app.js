734dfd19135bb1e150c72f1eaf1c2c5e59a0bc44
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-20 07:41:58 -0800

    required the deps and initialized the app

---
+++ app.js
@@ -0,0 +1,24 @@
+const express			= require('express');
+const path				= require('path');
+const cookieParser		= require('cookie-parser');
+const bodyParser		= require('body-parser');
+const exphbs			= require('express-handlebars');
+const expressValidator	= require('express-validator');
+const flash 			= require('connect-flash');
+const session			= require('express-session');
+const passport 			= require('passport');
+const localStrategy 	= require('passport-local').Strategy;
+const mongo				= require('mongodb');
+const mongoose			= require('mongoose');
+
+mongoose.connect('mongodb://ahmed_soliman:123456@ds143778.mlab.com:43778/passport-auth');
+const db 				= mongoose.connection;
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+app.listen(port, () => {
+	console.log(`Server is runing on port ${port}.`);
+});
