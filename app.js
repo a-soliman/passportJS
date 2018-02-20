17153f96150cecc3e0cc73f0396100bd9bf7972e
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-20 07:46:23 -0800

    set the view engine

--- app.js
+++ app.js
@@ -14,14 +14,21 @@ const mongoose			= require('mongoose');
 mongoose.connect('mongodb://ahmed_soliman:123456@ds143778.mlab.com:43778/passport-auth');
 const db 				= mongoose.connection;
 
+// Routes
 const routes 			= require('./routes/index');
 const users				= require('./routes/users');
 
+// Init App
 const app = express();
 const port = process.env.PORT || 3000;
 
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
 
 
+// Listen
 app.listen(port, () => {
 	console.log(`Server is runing on port ${port}.`);
 });
