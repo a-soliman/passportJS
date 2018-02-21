ed265013d08ddd47d903cc148f1600ec00d07c13
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-20 16:18:19 -0800

    coded the Passport Google auth functionality

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,61 @@
-const express		= require('express');
-const passport		= require('passport');
-const LocalStrategy	= require('passport-local').Strategy;
-const router		= express.Router();
-const User 			= require('../models/user');
+const express			= require('express');
+const passport			= require('passport');
+const LocalStrategy		= require('passport-local').Strategy;
+const GoogleStrategy	= require('passport-google-oauth20');
+const router			= express.Router();
+const User 				= require('../models/user');
 
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.getUserByUsername(username, ( err, user ) => {
-    	if ( err ) throw err;
-    	if ( !user ) {
-    		return done(null, false, { message: 'Unknown User.' })
-    	}
-
-    	User.comparePassword(password, user.password, ( err, isMatch ) => {
+  	function(username, password, done) {
+    	User.getUserByUsername(username, ( err, user ) => {
     		if ( err ) throw err;
-    		if( isMatch ) {
-    			return done(null, user);
+    		if ( !user ) {
+    			return done(null, false, { message: 'Unknown User.' })
     		}
-    		else {
-    			return done(null, false, { message: 'Invalid Password.'});
-    		}
-    	})
-    });
-      
-  }));
+
+    		User.comparePassword(password, user.password, ( err, isMatch ) => {
+    			if ( err ) throw err;
+    			if( isMatch ) {
+    				return done(null, user);
+    			}
+    			else {
+    				return done(null, false, { message: 'Invalid Password.'});
+    			}
+    		})
+    	});
+}));
+
+passport.use(
+	new GoogleStrategy({
+		// options for the google start
+		callbackURL: '/users/google/redirect/',
+		clientID: '821782046594-352pdc9ttf5j884p2sb8ml1e4peh20dr.apps.googleusercontent.com',
+		clientSecret: 'TJrlAv6Mr0wPk2NCinEZKtBS'
+	}, ( accessToken, refreshToken, profile, done ) => {
+		// passport callback function
+		let newUser = new User({
+			username: profile.displayName,
+			googleId: profile.id,
+			email: profile.emails[0].value
+		})
+		
+		//check if user already exists
+		User.findOne({googleId: profile.id}).then((currentUser) => {
+			if( currentUser ) {
+				// already have the user
+				console.log('=== this user already exists ===');
+				done(null, currentUser);
+			} else {
+				newUser.save().then((newUser) => {
+					console.log('NewUser created, ', newUser);
+					done(null, newUser);
+				})
+			}
+		})
+	})
+)
+
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
@@ -40,6 +71,14 @@ passport.deserializeUser(function(id, done) {
 // GET Login
 router.get('/login', ( req, res ) => {
 	res.render('login');
+});
+
+router.get('/google', passport.authenticate('google', {
+	scope: ['profile', 'email']
+}));
+
+router.get('/google/redirect', passport.authenticate('google'), ( req, res ) => {
+ res.redirect('/')
 });
 
 // POST Login
