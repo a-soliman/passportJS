42540cd913ba7820498a7f371e7ff2129b4f2c08
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-20 16:17:39 -0800

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ const express		= require('express');
 const router		= express.Router();
 
 //Get homepage
-router.get('/', ( req, res, next ) => {
+router.get('/', ensureAuthenticated, ( req, res, next ) => {
 	res.render('index');
 });
+
+function ensureAuthenticated ( req, res, next ) {
+	if ( req.isAuthenticated() ) {
+		return next();
+	}
+	else {
+		req.flash('error_msg', 'You are not loggedin.');
+		res.redirect('/users/login');
+	}
+}
 
 module.exports 	= router;
